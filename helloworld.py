ac8332412fa19e56ef6ed93545dd4c78083fdbe1
Digital-Lc <teichartel@wgc.school.nz>
2019-02-15 10:08:19 +1300

    Labels

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,15 @@
 import tkinter
-
 root = tkinter.Tk()
+
 root.title("Hello World!")
 root.geometry("600x200")
+
+my_label = tkinter.Label(root)
+my_label.config(text="WE ARE ALL GOING TO DIE", font=("Comic Sans MS", "50"), fg="white", bg="light pink")
+my_label.grid()
+
+my_label = tkinter.Label(root)
+my_label.config(text="Have a good day", fg="white", bg="light pink")
+my_label.grid()
 
 root.mainloop()
