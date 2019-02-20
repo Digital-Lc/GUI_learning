8c3ec15772e255ee7630259b3d425cd4399c1ac9
Digital-Lc <teichartel@wgc.school.nz>
2019-02-20 14:18:55 +1300

    added button + colour

--- change_labels.py
+++ change_labels.py
@@ -11,16 +11,20 @@ labeltext = StringVar()
 labeltext.set("")
 
 button1 = Button(root)
-button1.config(text="Say hello!", command=lambda: set_label1("hello"))
+button1.config(text="Say hello!", command=lambda: set_label1("hello"), bg="Blue", fg="White", font=("Skia", 50))
 button1.grid()
 
 button2 = Button(root)
-button2.config(text="Say banana!", command=lambda: set_label1("banana ಠ_ಠ"))
+button2.config(text="Say banana!", command=lambda: set_label1("banana ಠ_ಠ"), bg="Blue", fg="White", font=("Skia", 50))
 button2.grid()
 
 button3 = Button(root)
-button3.config(text="Clear text", command=lambda: set_label1(""))
+button3.config(text="Clear text", command=lambda: set_label1(""), bg="Blue", fg="White", font=("Skia", 50))
 button3.grid()
+
+button4 = Button(root)
+button4.config(text="please end me", command=lambda: set_label1("thank you"), bg="Blue", fg="Black", font=("Skia", 9))
+button4.grid()
 
 label1 = Label(root)
 label1.config(textvariable=labeltext)
