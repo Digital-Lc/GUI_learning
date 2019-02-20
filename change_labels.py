4573ef7141ff855218e3e097edef9cdfd09f8d21
Digital-Lc <teichartel@wgc.school.nz>
2019-02-20 14:05:11 +1300

    Lambda arguments added

--- change_labels.py
+++ change_labels.py
@@ -3,31 +3,23 @@ from tkinter import *
 root = Tk()
 
 
-def hello():
-    labeltext.set("Hello!")
-
-
-def banana():
-    labeltext.set("Banana :/")
-
-
-def clear():
-    labeltext.set("")
+def set_label1(text):
+    labeltext.set(text)
 
 
 labeltext = StringVar()
 labeltext.set("")
 
 button1 = Button(root)
-button1.config(text="Say hello!", command=hello)
+button1.config(text="Say hello!", command=lambda: set_label1("hello"))
 button1.grid()
 
 button2 = Button(root)
-button2.config(text="Say banana!", command=banana)
+button2.config(text="Say banana!", command=lambda: set_label1("banana ಠ_ಠ"))
 button2.grid()
 
 button3 = Button(root)
-button3.config(text="Clear text", command=clear)
+button3.config(text="Clear text", command=lambda: set_label1(""))
 button3.grid()
 
 label1 = Label(root)
