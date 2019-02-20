7498e567d25093a96938eefa583a6df34aedbbd7
Digital-Lc <teichartel@wgc.school.nz>
2019-02-20 14:38:29 +1300

    Functions added

--- calculator.py
+++ calculator.py
@@ -3,4 +3,20 @@ from tkinter import *
 root = Tk()
 root.title("Calculator")
 
+input_text = StringVar()
+input_text.set("")
+
+answer_value = StringVar()
+answer_value.set("")
+
+def add_to_input_text(next_val):
+    current_text = input_text.get()
+    input_text.set(current_text+next_val)
+
+def answer():
+    ans = eval(input_text.get())
+    answer_value.set(ans)
+
+def clear():
+
 root.mainloop()
